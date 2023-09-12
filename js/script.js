b5a55a1949897377e46b8d28e0dcb73e088c0bea
luizmiguelvr <luizmiguel.vr@gmail.com>
2023-09-12 17:50:24 -0400

    atualizações media

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick =() => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 // scroll sections
 let sections = document.querySelectorAll('section');
